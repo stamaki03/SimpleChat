c2ee52b57f8cf35d304b9e5cffc8349b5f56ce48
stamaki03 <stamaki03@gmail.com>
2023-09-18 10:05:30 +0900

    Initial Commit

---
+++ SimpleChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleChat
+//
+//  Created by Sho Tamaki on 2023/09/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
